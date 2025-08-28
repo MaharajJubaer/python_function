e3fa3c3857c83432a337d874b88b0d419bd8da8f
MaharajJubaer <maharajjubaer@gmail.com>
2025-08-28 10:10:12 +0600

    define function coding

---
+++ funtion.py
@@ -0,0 +1,12 @@
+#write a function to print the length of a list(list is the parameter)
+
+list_data = ["tara","lavender","roohi","sweetheart"]
+list_data1 = ["rakib","no_one"]
+
+def length(list):
+    a = len(list)
+    print(a)
+    return a
+
+length(list_data)
+length(list_data1)
