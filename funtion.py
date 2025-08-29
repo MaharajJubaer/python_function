f0ed5998d4be5e035f8a3ab3f72548d3539766ae
MaharajJubaer <maharajjubaer@gmail.com>
2025-08-29 19:49:41 +0600

    recursive function

--- funtion.py
+++ funtion.py
@@ -1,15 +1,15 @@
 #write a function to print the length of a list(list is the parameter)
 
-# list_data = ["tara","lavender","roohi","sweetheart"]
-# list_data1 = ["rakib","no_one"]
+list_data = ["tara","lavender","roohi","sweetheart"]
+list_data1 = ["rakib","no_one"]
 
-# def length(list):
-#     a = len(list)
-#     print(a)
-#     return a
+def length(list):
+    a = len(list)
+    print(a)
+    return a
 
-# length(list_data)
-# length(list_data1)
+length(list_data)
+length(list_data1)
 
 #WAF to print the elements of a list in a single line(list is a parameter)
 
@@ -49,6 +49,18 @@ def convert(USD):
 convert(5)
 convert(10)
 convert(15)
+
+#writea recursive function to calculate the sum of first natural number
+
+def calc_sum(n):
+    if(n==0):
+        return 0
+    calc_sum(n-1)
+    sum = calc_sum(n-1) + n
+    return sum
+
+print(calc_sum(5))
+
 
 
 
